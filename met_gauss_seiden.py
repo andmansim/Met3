0aca482290387e0d1a07759294ccf58c5d283d3c
andmansim <91722324+andmansim@users.noreply.github.com>
2024-02-21 17:32:25 +0100

    cc

--- met_gauss_seiden.py
+++ met_gauss_seiden.py
@@ -1,12 +1,12 @@
 #metodo de Gauss Seiden
 
 #Extremos de x, y
-a = int(input("Ingrese el valor de a: "))
-b = int(input("Ingrese el valor de b: "))
-c = int(input("Ingrese el valor de c: "))
-d = int(input("Ingrese el valor de d: "))
+a = int(input("Ingrese el valor de a, x0: "))
+b = int(input("Ingrese el valor de b, xf: "))
+c = int(input("Ingrese el valor de c, y0: "))
+d = int(input("Ingrese el valor de d, yf: "))
 
-#Número de huecos de x, y
+#Número de huecos de x, y. Los elegimos nosotros
 N = int(input("Ingrese el número de huecos de X: "))
 M = int(input("Ingrese el número de huecos de Y: "))
 
@@ -15,25 +15,32 @@ h = (b-a)/N
 k = (d-c)/M 
 
 #Matriz
-w = [[]]
+# Inicializar matriz w con dimensiones N x M
+w = [[0 for j in range(M)] for i in range(N)]
 
-#Creamos los bordes de la matriz
-for i in range(1, N):
-    w[i][0] = 
-    w[i][M] = 
 
-for j in range(1, M):
-    w[0][j] = 
-    w[N][j] = 
+#funcion
+def f(x, y):
+    return None
 
 #Rellenamos el resto de la matriz
 for i in range(N):
     for k in range(M):
         w[i][k] = 0
 
-#funcion
-def f(x, y):
-    return 0
+#Creamos los bordes de la matriz
+for i in range(1, N):
+    #Cambian dependiendo del ejercicio
+    print(i)
+    w[i][0] = 0
+    w[i][M] = 0
+
+for j in range(1, M):
+    #Cambian dependiendo del ejercicio
+    w[0][j] = 0
+    w[N][j] = 1
+
+
 
 #recorremos los puntos interiores de la malla
 for k in range(100):#iteramos inicialmente 100, luego ya lo adapataremos
@@ -42,6 +49,8 @@ for k in range(100):#iteramos inicialmente 100, luego ya lo adapataremos
             w[i][j] = ( k**2 * (w[i+1][j]+w[i-1][j])+ h**2*(w[i][j+1]+w[i][j-1]) - (h*k)**2 * f(i, j))/(2*(h**2+k**2))
             
 '''
-Ejemplo:
-
+Ejemplo: Laplaciano, la A es el gradiente
+Au = 0 0<x<1, 0<y<1
+u(0,y) = u(x,0) = u(x,1) = 0
+u(1,y) = 1
 '''
