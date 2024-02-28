42d32765782c5ccf2546e9be5a30eaa5708abf4d
andmansim <91722324+andmansim@users.noreply.github.com>
2024-02-28 16:44:02 +0100

    nn

--- met_gauss_seiden.py
+++ met_gauss_seiden.py
@@ -40,7 +40,7 @@ for j in range(1, M):
 
 
 #recorremos los puntos interiores de la malla
-for k in range(100):#iteramos inicialmente 100, luego ya lo adapataremos
+for o in range(100):#iteramos inicialmente 100, luego ya lo adapataremos
     for i in range(1, N):
         for j in range(1, M):
             w[i][j] = ( k**2 * (w[i+1][j]+w[i-1][j])+ h**2*(w[i][j+1]+w[i][j-1]) - (h*k)**2 * f(i, j))/(2*(h**2 + k**2))
