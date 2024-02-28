b957cf9b31fdc53647bccbd733d378ae77b7d657
andmansim <91722324+andmansim@users.noreply.github.com>
2024-02-28 17:22:51 +0100

    ffff

--- met_gauss_seiden.py
+++ met_gauss_seiden.py
@@ -27,14 +27,14 @@ def f(x, y):
 #Creamos los bordes de la matriz
 for i in range(1, N):
     #Cambian dependiendo del ejercicio
-    #las x son las i
+    #las x = (a+i*h)
     w[i][0] = 0
-    w[i][M] = 0
+    w[i][M] = (a+i*h)**2
 
 for j in range(1, M):
     #Cambian dependiendo del ejercicio
-    #las y son las j
-    w[0][j] = 0
+    #las y = (c+j*k)
+    w[0][j] = 1-(c+k*j)**2
     w[N][j] = 1
 
 
@@ -43,9 +43,13 @@ for j in range(1, M):
 for o in range(100):#iteramos inicialmente 100, luego ya lo adapataremos
     for i in range(1, N):
         for j in range(1, M):
-            w[i][j] = ( k**2 * (w[i+1][j]+w[i-1][j])+ h**2*(w[i][j+1]+w[i][j-1]) - (h*k)**2 * f(i, j))/(2*(h**2 + k**2))
+            w[i][j] = ( k**2 * (w[i+1][j] + w[i-1][j])+ h**2 * (w[i][j+1] + w[i][j-1]) - (h*k)**2 * f(i, j))/(2*(h**2 + k**2))
             
 '''
+Nos va a preguntar la interpretación gráfica de las EDP
+El laplaciano minimiza la energía, es decir, el calor se va a ir a los bordes. 
+Un ejemplo de esto es una lona estirada, donde al tensionar dandole una forma no horizontal, 
+esta se quede da una forma que minimiza la energía. 
 Eje1: Laplaciano, la A es la divergencia del gradiente, la A = gradiente ^2
 Au = 0 0<x<1, 0<y<1
 u(0,y) = u(x,0) = u(x,1) = 0
