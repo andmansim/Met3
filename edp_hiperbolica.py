6170a65d06cd822a28676ff6ee6035ac18bc53f0
andmansim <91722324+andmansim@users.noreply.github.com>
2024-03-05 16:58:12 +0100

    hiperbolicas

---
+++ edp_hiperbolica.py
@@ -0,0 +1,80 @@
+#Edp hiperbólica
+import numpy as np
+
+#Extremos de x, y
+a = 0
+b = int(input("Ingrese el valor de b, xf: "))
+c = 0 
+d = int(input("Ingrese el valor de d, yf: "))
+#velocidad
+v = int(input("Ingrese la velocidad: "))
+#Número de huecos de x, y. Los elegimos nosotros
+N = int(input("Ingrese el número de huecos de X: "))
+M = int(input("Ingrese el número de huecos de Y: "))
+
+
+#Tamaño de los huecos
+h = (b)/N
+k = (d)/M 
+p = v*h/k #para no escribir todo esto abajo
+
+#Matriz
+# Inicializar matriz w con dimensiones N x M
+w = [[0 for j in range(M+1)] for i in range(N+1)]
+
+
+#funcion inicial y de frontera
+def f(x):
+    #Cambia dependiendo del ejercicio
+    return 0
+
+def g(x):  
+    #Cambia dependiendo del ejercicio
+    return 0
+
+#Creamos los bordes de la matriz
+for i in range(1, N):
+    #Cambian dependiendo del ejercicio
+    w[0][i] = f(h*i)
+    w[M][i] = w[0][i] + k*g(h*i)
+
+for j in range(1, M):
+    #Cambian dependiendo del ejercicio
+    w[j][0] = 0
+    w[j][N] = 0
+
+
+
+#recorremos los puntos interiores de la malla
+for i in range(1, N):
+    for j in range(1, M):
+        w[j+1][i] = 2*(1 - p**2)*w[j][i] + (p**2)*(w[j][i+1] + w[j][i-1]) - w[j-1][i]
+        
+'''
+
+'''
+
+#Mostramos la grafica de la matriz
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+
+#definir coordenadas
+x = np.linspace(0, b, N+1)
+y = np.linspace(0, d, M+1)
+X, Y = np.meshgrid(x, y)
+Z = np.array(w)
+
+#graficar
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.plot_surface(X, Y, Z, cmap='viridis')
+
+#etiquetas
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Z')
+
+#mostramos
+plt.show()
+
