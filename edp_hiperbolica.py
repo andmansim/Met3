9b43b165815d8c651691fc21802e8bb5ec9d20f8
andmansim <91722324+andmansim@users.noreply.github.com>
2024-03-06 17:08:28 +0100

    d

--- edp_hiperbolica.py
+++ edp_hiperbolica.py
@@ -16,20 +16,20 @@ M = int(input("Ingrese el número de huecos de T: "))
 
 
 #Tamaño de los huecos
-h = (b-a)/N
-k = (d-c)/M 
-p = (v*k)/h #para no escribir todo esto abajo
+h = (b - a) / N
+k = (d - c) / M 
+p = (v * k) / h #para no escribir todo esto abajo
 
 #Matriz
 # Inicializar matriz w con dimensiones N x M
-w = [[0 for j in range(M+1)] for i in range(N+1)]
+w = [[0 for j in range(M + 1)] for i in range(N + 1)]
 
 
 #funcion inicial y de frontera
 def f(x):
     #Cambia dependiendo del ejercicio
     #x = (a+i*h)
-    return x*(b-x)
+    return x * (b - x)
 
 def g(x):  
     #Cambia dependiendo del ejercicio
@@ -38,13 +38,13 @@ def g(x):
 #Creamos los bordes de la matriz
 for i in range(1, N):
     #Cambian dependiendo del ejercicio
-    w[0][i] = f(a + h*i)
-    w[1][i] = w[0][i] + k*g(a + h*i)
+    w[i][0] = f(a + h*i)
+    w[i][1] = w[i][0] + k * g(a + h*i)
 
 for j in range(1, M):
     #Cambian dependiendo del ejercicio
-    w[j][0] = 0
-    w[j][N] = 0
+    w[0][j] = 0
+    w[N][j] = 0
 
 
 
@@ -52,15 +52,20 @@ for j in range(1, M):
 #for u in range(100):
     for j in range(1, M):
         for i in range(1, N):
-                w[j+1][i] = 2*(1-p**2)*w[j][i] + (p**2)*(w[j][i+1] + w[j][i-1]) - w[j-1][i]
+                w[i][j+1] = 2*(1-p**2)*w[i][j] + (p**2)*(w[i+1][j] + w[i-1][j]) - w[i][j-1]
             
 '''
+hay una velocidad máxima, donde es h/k a partir de esta velocidad el método no es estable
+Siempre hay que calcularla para no pasarnos de ella
 Ec de ondas
 Eje1: 
     f(x) = x(b-x)
     g(x) = 0
     b=5, d = 10, v=0.5, N=40, M=400 
 
+Eje2:
+    f(x) = x     x < b/2
+    f(x) = b-x   x > b/2
 '''
 
 #Mostramos la grafica de la matriz
